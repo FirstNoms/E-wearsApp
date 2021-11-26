c6131d415cccac979208ff02eab5b3a914729512
Confidence Okere <okerenomso@gmail.com>
2021-11-26 01:25:34 +0100

    started an e-wears application using spring boot

---
+++ src/main/java/com/africa/ewearsapp/EWearsAppApplication.java
@@ -0,0 +1,13 @@
+package com.africa.ewearsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWearsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EWearsAppApplication.class, args);
+    }
+
+}
